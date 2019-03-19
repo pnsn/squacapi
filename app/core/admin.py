05447b2ca17db9bfb6dd1b049bdd58ab549bc833
Jon Connolly <jontconnolly@gmail.com>
2019-03-19 13:17:03 -0700

    fix flake crap to get ci to pass

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from core import models
 
-# Register your models here.
+admin.site.register(models.Site)
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+
+class Site(models.Model):
+    pass
+
 # Create your models here.
