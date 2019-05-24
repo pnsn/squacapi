7ab17cb085c6d1f7017a8f430f671a0d67c41588
Jon Connolly <jontconnolly@gmail.com>
2019-05-24 14:43:45 -0700

    comply with the damn linter

--- app/core/models.py
+++ app/core/models.py
@@ -35,5 +35,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     objects = UserManager()
-    
     USERNAME_FIELD = 'email'
