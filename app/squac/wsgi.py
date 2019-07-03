418d21630b38014bc6d738664e286848d3d0d8a1
Jon Connolly <jontconnolly@gmail.com>
2019-07-02 17:10:32 -0700

    modify wsgi for python path, correct app name to squac, add django-extensions

--- app/squac/wsgi.py
+++ app/squac/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
+import sys
+# there is probably a better way of doing this but without we get the module 
+# squac not found
+# include full path up to app/
+file_path = os.path.abspath(os.path.dirname(__file__))
+file_list = file_path.split("/")
+app_path = "/".join(file_list[0:-1])
+sys.path.insert(0, os.path.abspath(app_path))
+
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'squac.settings')
 
 application = get_wsgi_application()
