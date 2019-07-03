7d9195c7e011628ad48d3b6d2e209004fbbdab46
Jon Connolly <jontconnolly@gmail.com>
2019-07-03 12:15:29 -0700

    clean up syntax to pass travis

--- app/squac/wsgi.py
+++ app/squac/wsgi.py
@@ -9,16 +9,14 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 
 import os
 import sys
-# there is probably a better way of doing this but without we get the module 
+from django.core.wsgi import get_wsgi_application
+# there is probably a better way of doing this but without we get the module
 # squac not found
 # include full path up to app/
 file_path = os.path.abspath(os.path.dirname(__file__))
 file_list = file_path.split("/")
 app_path = "/".join(file_list[0:-1])
 sys.path.insert(0, os.path.abspath(app_path))
-
-
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'squac.settings')
 
