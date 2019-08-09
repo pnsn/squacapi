aea0c032261d70c2a798e842e57b744bdafb3b73
Jon Connolly <jontconnolly@gmail.com>
2019-08-08 21:49:23 -0700

    remove v1.0 from url of user

--- app/squac/urls.py
+++ app/squac/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('v1.0/user/', include('user.urls')),
+    path('user/', include('user.urls')),
     path('v1.0/nslc/', include('nslc.urls')),
     path('v1.0/measurement/', include('measurement.urls'))
 ]
