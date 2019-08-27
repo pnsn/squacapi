96aeeaccf8f573fa665c0acbde9c8744f3764ab7
Jon Connolly <jontconnolly@gmail.com>
2019-08-26 17:35:00 -0700

    more merging

--- app/dashboard/serializers.py
+++ app/dashboard/serializers.py
@@ -63,23 +63,15 @@ class DashboardDetailSerializer(DashboardSerializer):
         read_only_fields = ('id',)
 
 
-<<<<<<< HEAD
-class WidgetTypeSerializer(serializers.HyperlinkedModelSerializer):
-=======
 class WidgetDetailSerializer(WidgetSerializer):
     dashboard = DashboardSerializer(read_only=True)
     widgettype = WidgetTypeSerializer(read_only=True)
     metrics = MetricSerializer(many=True, read_only=True)
->>>>>>> daa99e54852aa1951005512043b5179e3e762223
 
     class Meta:
         model = Widget
         fields = (
-<<<<<<< HEAD
-            'id', 'name', 'type', 'created_at', 'updated_at'
-=======
             'id', 'name', 'dashboard', 'widgettype', 'metrics',
             'created_at', 'updated_at'
->>>>>>> daa99e54852aa1951005512043b5179e3e762223
         )
         read_only_fields = ('id',)
