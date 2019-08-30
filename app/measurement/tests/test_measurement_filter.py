bc1a6736ce22a17d06050ffb37f86cf340c3c9af
jtclayt <jt.clayton92@yahoo.com>
2019-08-30 09:42:04 -0700

    Changed fixture file name, changed filter pks to match new fixture file.

--- app/measurement/tests/test_measurement_filter.py
+++ app/measurement/tests/test_measurement_filter.py
@@ -9,7 +9,7 @@ from django.urls import reverse
 
 class PublicMeasurementFilterTests(TestCase):
     # Public tests for filters
-    fixtures = ['measurement_tests.json']
+    fixtures = ['fixtures_all.json']
     # Fixtures load from fixture directory within measurement app
 
     def setUp(self):
@@ -28,11 +28,11 @@ class PublicMeasurementFilterTests(TestCase):
         # Test that filter properly finds the measurement that fits params
         url = reverse('measurement:measurement-list')
         stime, etime = '2018-02-01T03:00:00Z', '2018-02-02T05:00:00Z'
-        url += f'?metric=284&channel=4430&starttime={stime}&endtime={etime}'
+        url += f'?metric=2&channel=5&starttime={stime}&endtime={etime}'
         res = self.client.get(url)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
-        self.assertEqual(res.data[0]['id'], 6601)
+        self.assertEqual(res.data[0]['id'], 1)
 
     def test_measurement_out_of_range_filter(self):
         # Test that filter does not return measurements outside date range
