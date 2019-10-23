c48943fbabe443b0c80790e04236629e08d46459
Jon Connolly <jontconnolly@gmail.com>
2019-10-22 17:17:12 -0700

    add metrics to admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from core import models
+from measurement.models import Metric
 
 
 class UserAdmin(BaseUserAdmin):
---
+++ app/measurement/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Metric
+
+admin.site.register(Metric)
