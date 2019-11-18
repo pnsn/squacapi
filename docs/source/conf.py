d856b37e26ab81ff704d2b211890042b3f792d34
Jon Connolly <jontconnolly@gmail.com>
2019-11-18 15:27:30 -0800

    add sphinx documentation, deploy script

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Squacapi'
+project = 'SQUACapi'
 copyright = '2019, Jon Connolly'
 author = 'Jon Connolly'
 
@@ -111,7 +111,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -119,7 +119,7 @@ html_theme = 'alabaster'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ["_themes"]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
