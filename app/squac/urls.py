c179f9b7a1630c31794d4bff6e8553cf119f3dfe
Aditya Kumar <KumarAdi@users.noreply.github.com>
2020-01-03 09:55:49 -0800

    Added swagger docs to api
    Can be used to auto-generate client sdks and do validations

--- app/squac/urls.py
+++ app/squac/urls.py
@@ -14,9 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from . import views
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Squac API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 
 urlpatterns = [
@@ -29,8 +46,13 @@ urlpatterns = [
     path('v1.0/measurement/', include('measurement.urls')),
     path('v1.0/dashboard/', include('dashboard.urls')),
     path('api-auth/', include('rest_framework.urls',
-         namespace='rest_framework'))
-
+         namespace='rest_framework')),
+    path('swagger/',
+         schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
 ]
 
 if settings.DEBUG:
