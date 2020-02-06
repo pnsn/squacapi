3ececf6c920e5ad0118aef77002877757812763e
Jon Connolly <jontconnolly@gmail.com>
2020-02-06 10:54:14 -0800

    add to our ever expanding list of allowed hosts

--- app/squac/settings.py
+++ app/squac/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ.get('SQUAC_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('SQUAC_DEBUG_MODE') == 'True',
 
-ALLOWED_HOSTS = ['squac.pnsn.org', 'squacapi.pnsn.org', 'localhost']
+ALLOWED_HOSTS = ['squac.pnsn.org', 'squacapi.pnsn.org', 'localhost',
+                 'staging-squacapi.pnsn.org','staging-squac.pnsn.org']
 
 
 # For debug toolbar
