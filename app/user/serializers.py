d1821acd9963558c659b8eb3fdafbfa5553430c4
Jon Connolly <jontconnolly@gmail.com>
2020-03-09 14:05:51 -0700

    whoops forgot to update tests

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'password', 'firstname', 'lastname', 'is_staff', 
+        fields = ('email', 'password', 'firstname', 'lastname', 'is_staff',
                   'organization', 'groups',)
         read_only_fields = ('is_staff',)
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
