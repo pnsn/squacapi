8a6da440e7e5ff5b723ef02292ceb7ff212e7aaf
Jon Connolly <jontconnolly@gmail.com>
2020-03-10 11:43:13 -0700

    better comments

--- app/user/apps.py
+++ app/user/apps.py
@@ -4,5 +4,6 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
     name = 'user'
 
+    # this is how we load the reciever
     def ready(self):
         from user import signals  # noqa
