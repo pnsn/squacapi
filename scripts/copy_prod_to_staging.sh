f6fd4256afd708ca9cf167ab334ff18956e77611
Jon Connolly <jontconnolly@gmail.com>
2020-10-20 16:40:00 -0700

    add docker routine to copy prod to staging

--- scripts/copy_prod_to_staging.sh
+++ scripts/copy_prod_to_staging.sh
@@ -1,20 +1,62 @@
 #!/usr/bin/env bash
 # dump all production data except full measurementes from prod to staging
-path=/usr/pgsql-10/bin
 #how many of interval
 num=2
 #interval
 interval=hours
-$path/dropdb squacapi_staging
-$path/createdb squacapi_staging
 
-$path/pg_dump --schema-only squacapi_production | psql -d squacapi_staging
+docker run -it --rm \
+--env PGPASSWORD=$SQUAC_DB_PASS  \
+postgres:12.4-alpine \
+dropdb -h $SQUAC_DB_HOST  -U $SQUAC_DB_USER squacapi_staging
 
-$path/pg_dump --data-only --exclude-table=measurement_measurement --exclude-table=measurement_archive  squacapi_production | \
-psql -d squacapi_staging
+docker run -it --rm \
+--env PGPASSWORD=$SQUAC_DB_PASS  \
+postgres:12.4-alpine \
+createdb -h $SQUAC_DB_HOST  -U $SQUAC_DB_USER squacapi_staging
 
-$path/psql -d squacapi_production -c "COPY(SELECT * FROM measurement_measurement  WHERE starttime > now() - interval '$num $interval') to STDOUT;" | \
-$path/psql -d squacapi_staging -c "COPY measurement_measurement FROM  STDIN;"
 
-$path/psql -d squacapi_production -c "COPY(SELECT * FROM measurement_archive  WHERE starttime  > now() - interval '$num $interval') to STDOUT;" | \
-$path/psql -d squacapi_staging -c "COPY measurement_archive FROM  STDIN;"
+docker run -it --rm \
+--env PGPASSWORD=$SQUAC_DB_PASS  \
+postgres:12.4-alpine \
+pg_dump -h $SQUAC_DB_HOST  -U $SQUAC_DB_USER squacapi_production > \
+/tmp/prod.sql
+
+docker run -it --rm \
+--env PGPASSWORD=$SQUAC_DB_PASS  \
+--mount type=bind,source=/tmp,target=/tmp \
+postgres:12.4-alpine \
+psql -h $SQUAC_DB_HOST  -U $SQUAC_DB_USER squacapi_staging \
+-f /tmp/prod.sql
+
+
+docker run -it --rm \
+--env PGPASSWORD=$SQUAC_DB_PASS  \
+postgres:12.4-alpine \
+pg_dump -h $SQUAC_DB_HOST  -U $SQUAC_DB_USER squacapi_production \
+--exclude-table=measurement_measurement \
+--exclude-table=measurement_archive  \
+> /tmp/data.sql
+
+docker run -it --rm \
+--env PGPASSWORD=$SQUAC_DB_PASS  \
+--mount type=bind,source=/tmp,target=/tmp \
+postgres:12.4-alpine \
+psql -h $SQUAC_DB_HOST  -U $SQUAC_DB_USER squacapi_staging \
+-f /data/data.sql
+
+
+# use stdout of one docker to update by stdin of other container. 
+docker run -it --rm \
+--env PGPASSWORD=$SQUAC_DB_PASS  \
+-a stdin -a stdout -a stderr \
+postgres:12.4-alpine \
+psql -h $SQUAC_DB_HOST  -U $SQUAC_DB_USER -d squacapi_production \
+-c "COPY(SELECT * FROM measurement_measurement  WHERE starttime > now() \
+    - interval '$num $interval') TO STDOUT;" | docker run -i --rm \
+--env PGPASSWORD=$SQUAC_DB_PASS  \
+-a stdin -a stdout -a stderr \
+postgres:12.4-alpine \
+psql -h $SQUAC_DB_HOST  -U $SQUAC_DB_USER squacapi_staging \
+-c "COPY measurement_measurement FROM  STDIN;"
+
