f3ae3cf53b1b4a8ab712b18be7beeca10ecc8e62
Jon Connolly <jontconnolly@gmail.com>
2020-10-27 12:43:10 -0700

    pluralize

--- app/squac/settings.py
+++ app/squac/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get('SQUAC_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('SQUAC_DEBUG_MODE') == 'True',
 try:
-    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST_LIST').split(',')
+    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS_LIST').split(',')
 except AttributeError:
     ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
