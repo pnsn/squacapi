7e74013ec6c012685f03f80585e64ae9fb483e59
Jon Connolly <jontconnolly@gmail.com>
2020-10-30 10:13:40 -0700

    add profiling middleware

--- app/squac/settings.py
+++ app/squac/settings.py
@@ -92,6 +92,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_cprofile_middleware.middleware.ProfilerMiddleware',
     'django.middleware.cache.FetchFromCacheMiddleware', #must be last!!
 
 ]
