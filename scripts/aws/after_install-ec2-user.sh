9e37df064fb9719ada3c3626014f83b1ae1ddeb9
Jon Connolly <jontconnolly@gmail.com>
2020-11-05 15:11:24 -0800

    fixed bad path refs

--- scripts/aws/after_install-ec2-user.sh
+++ scripts/aws/after_install-ec2-user.sh
@@ -3,7 +3,10 @@ set -e
 set -o pipefail
 dest=/var/www/releases/$DEPLOYMENT_GROUP_NAME/$DEPLOYMENT_ID
 mv /var/www/releases/tmp $dest
-cd $dest
+aws s3 cp s3://squacapi-config/bash/$DEPLOYMENT_GROUP_NAME.env  $dest/app/.env
+
+
+
 
 VIRTUALENVWRAPPER_PYTHON=/var/.virtualenvs/$DEPLOYMENT_GROUP_NAME/bin/python
 source `which virtualenvwrapper.sh`
