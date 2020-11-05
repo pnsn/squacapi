c87720a18eea4b386d58f6831de68cba9410a74d
Jon Connolly <jontconnolly@gmail.com>
2020-11-05 15:39:12 -0800

    chown to ec2-user

--- scripts/aws/after_install-root.sh
+++ scripts/aws/after_install-root.sh
@@ -9,3 +9,4 @@ aws s3 cp s3://squacapi-config/nginx/staging-squacapi.conf  /etc/nginx/conf.d/
 # install gunicorn service files
 aws s3 cp s3://squacapi-config/gunicorn/gunicorn-production.service /etc/systemd/system/
 aws s3 cp s3://squacapi-config/gunicorn/gunicorn-staging.service /etc/systemd/system/
+chown -R ec2-user:ec2-user /var/www/releases/tmp
