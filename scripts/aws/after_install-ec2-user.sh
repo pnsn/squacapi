428698bd7f8db775f38638d34d19723edab73ed8
Jon Connolly <jontconnolly@gmail.com>
2020-11-05 15:44:58 -0800

    source bash_profile

--- scripts/aws/after_install-ec2-user.sh
+++ scripts/aws/after_install-ec2-user.sh
@@ -7,7 +7,7 @@ aws s3 cp s3://squacapi-config/bash/$DEPLOYMENT_GROUP_NAME.env  $dest/app/.env
 
 
 
-
+source /home/ec2-user/.bash_profile
 VIRTUALENVWRAPPER_PYTHON=/var/.virtualenvs/$DEPLOYMENT_GROUP_NAME/bin/python
 source /usr/local/bin/virtualenvwrapper.sh
 workon $DEPLOYMENT_GROUP_NAME
