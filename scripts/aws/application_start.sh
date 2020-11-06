574e28433ee97b2d650a8a0f202942b054cd1b2f
Jon Connolly <jontconnolly@gmail.com>
2020-11-06 11:46:56 -0800

    configure travis for cd, remove set commands from afterinstall

--- scripts/aws/application_start.sh
+++ scripts/aws/application_start.sh
@@ -7,14 +7,14 @@ set -o pipefail
 dest=/var/www/releases/$DEPLOYMENT_GROUP_NAME/$DEPLOYMENT_ID
 symlink=/var/www/$DEPLOYMENT_GROUP_NAME
 # save previous pointer then repoint symlink
-export PREVIOUS_BUILD=`ls $symlink`
+PREVIOUS_BUILD=`ls -d $symlink`
 rm $symlink && ln -s $dest $symlink
 
 #try restarting if it doesn't work 
 #restart gunicorn
 if [ $DEPLOYMENT_GROUP_NAME == 'squacapi' ]; then
-    service gunicorn-production restart || rm $symlink && ln -s $PREVIOUS_BUILD $symlink
+    service gunicorn-production restart || rm $symlink && ln -s $PREVIOUS_BUILD $symlink && echo 'prod fail'
 else
-    service gunicorn-staging restart || rm $symlink && ln -s $PREVIOUS_BUILD $symlink
+    service gunicorn-staging restart || rm $symlink && ln -s $PREVIOUS_BUILD $symlink && echo 'staging fail'
 fi
 
