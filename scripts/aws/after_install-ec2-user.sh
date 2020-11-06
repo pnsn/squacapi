4f3c89dfea75245c88ae3980b6a59bad9a75dd39
Jon Connolly <jontconnolly@gmail.com>
2020-11-05 17:41:50 -0800

    remove set calls for debugging

--- scripts/aws/after_install-ec2-user.sh
+++ scripts/aws/after_install-ec2-user.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-set -e
-set -o pipefail
+# set -e
+# set -o pipefail
 dest=/var/www/releases/$DEPLOYMENT_GROUP_NAME/$DEPLOYMENT_ID
 mv /var/www/releases/tmp $dest
 aws s3 cp s3://squacapi-config/bash/$DEPLOYMENT_GROUP_NAME.env  $dest/app/.env
 
 # virtual env vars
-export WORKON_HOME=/var/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-echo 'here'
-source `which virtualenvwrapper.sh`
-echo 'wtf'
+WORKON_HOME=/var/.virtualenvs
+VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+source /usr/local/bin/virtualenvwrapper.sh
 source $dest/app/.env
 echo $DEPLOYMENT_GROUP_NAME
-workon $DEPLOYMENT_GROUP_NAME 2>&1
-echo 'now here'
+workon $DEPLOYMENT_GROUP_NAME
 echo "I can't believe I have to debug this way"
-pip3 install  -r $dest/requirements/production.txt 2>&1
-python $dest/app/manage.py migrate 2>&1
-python $dest/app/manage.py collectstatic --noinput 2>&1
+pip3 install  -r $dest/requirements/production.txt
+python $dest/app/manage.py migrate
+python $dest/app/manage.py collectstatic --noinput
 echo "this is crap"
-deactivate 2>&1
+deactivate
 
 
 
