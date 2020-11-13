1c2b2bcdb42cbcaa214c9c02cd7052eed87db656
Jon Connolly <jontconnolly@gmail.com>
2020-11-13 14:33:43 -0800

    remove set calls

--- scripts/aws/after_install-ec2-user.sh
+++ scripts/aws/after_install-ec2-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
-set -o pipefail
+# set -e
+# set -o pipefail
 dest=/var/www/releases/$DEPLOYMENT_GROUP_NAME/$DEPLOYMENT_ID
 mv /var/www/releases/tmp $dest
 aws s3 cp s3://squacapi-config/bash/$DEPLOYMENT_GROUP_NAME.env  $dest/app/.env
