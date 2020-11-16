2687cf6c39c7b207e4b59c2adef742d724f30024
jtclayt <jt.clayton92@yahoo.com>
2020-11-16 10:42:05 -0800

    Add helper functions for loading recent measurement data

--- app/core/management/commands/bootstrap_db.py
+++ app/core/management/commands/bootstrap_db.py
@@ -1,11 +1,25 @@
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
+import requests
+import os
 
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
+    def load_sample_hourly_metric(self):
+        ''' Populates several days of hourly_mean (id=84) metric for several
+            stations
+        '''
+        pass
+
+    def load_sample_latency_metric(self):
+        ''' Populates several days of latency metric for several stations '''
+        pass
+
+    def handle(self, *args, **kwargs):
         ''' This command flushes all data from db and reloads data from
             fixture files, collects recent measurements from production
         '''
         call_command('flush', '--noinput')
         call_command('loaddata', 'fixtures/fixtures_all.json')
+        self.load_sample_hourly_metric()
+        self.load_sample_latency_metric()
