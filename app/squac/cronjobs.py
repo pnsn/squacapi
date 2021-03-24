93d462d378ccc16d2bf40fd264ecd6166af764c9
ulbergc <ulbergc@uw.edu>
2021-03-24 14:19:41 -0700

    Add s3_query_export command to cronjobs

--- app/squac/cronjobs.py
+++ app/squac/cronjobs.py
@@ -4,5 +4,6 @@ CRONJOBS=[ # noqa
     ('0 10 * * *', 'django.core.management.call_command', ['load_from_fdsn']),
     ('0 20 * * *', 'django.core.management.call_command',
         ['create_table_partition']),
-    ('5 * * * *', 'django.core.management.call_command', ['evaluate_alarms'])
+    ('5 * * * *', 'django.core.management.call_command', ['evaluate_alarms']),
+    ('0 5 * * *', 'django.core.management.call_command', ['s3_query_export'])
 ]
