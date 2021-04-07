19fbe25c6280aa08422da481fa0f6ccbf6ad7feb
ulbergc <ulbergc@uw.edu>
2021-04-07 10:26:46 -0700

    Update s3 export query to backup past 3 days by default

--- app/squac/cronjobs.py
+++ app/squac/cronjobs.py
@@ -1,15 +1,8 @@
 '''
-List all application  crons here. These are imported by settings.
+List all application crons here. These are imported by settings.
 Verify they look correct with 'python {squac_dir}/app/manage.py crontab show'
 Reference: https://pypi.org/project/django-crontab/
 '''
-from datetime import datetime, timedelta
-import pytz
-
-# Data will be exported (and clobbered) for every day going back N days
-S3_EXPORT_N_DAYS = 3
-s3_export_startdate = (
-    datetime.now(tz=pytz.utc) - timedelta(days=S3_EXPORT_N_DAYS)).date()
 
 CRONJOBS=[ # noqa
     ('0 10 * * *', 'django.core.management.call_command', ['load_from_fdsn']),
