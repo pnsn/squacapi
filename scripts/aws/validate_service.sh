89e0fa7edbca6a54275620f7ddfb161dec0c2527
Kyla Marczewski <marczk@uw.edu>
2021-08-04 12:03:59 -0700

    increase timeout

--- scripts/aws/validate_service.sh
+++ scripts/aws/validate_service.sh
@@ -5,27 +5,10 @@ set -o pipefail
 #should get 401 (unauthenticate user)
 url=https://$DEPLOYMENT_GROUP_NAME.pnsn.org
 resp=`curl -sL -w "%{http_code}\\n" "$url" -o /dev/null`
-
-i=0
-while [[ $i -lt 5 ]]
-do
-  if [ $resp == 401 ];then
+if [ $resp == 401 ];then
     echo "Success"
-    break
-  else
+else
     echo "Not success - $resp"
-    ((i++))
-    sleep 15
-  fi
-done
-
-if [[ $i -eq 5 ]]; then
-  echo "Validate service failed after 5 attempts"
-  exit 1
+    exit 1
 fi
-
-
-
-
-
 
