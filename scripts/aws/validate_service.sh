862c9a3c49e90097003b87f95cbf4c646891989b
Kyla Marczewski <marczk@uw.edu>
2021-08-04 11:33:27 -0700

    add try to validate

--- scripts/aws/validate_service.sh
+++ scripts/aws/validate_service.sh
@@ -5,10 +5,26 @@ set -o pipefail
 #should get 401 (unauthenticate user)
 url=https://$DEPLOYMENT_GROUP_NAME.pnsn.org
 resp=`curl -sL -w "%{http_code}\\n" "$url" -o /dev/null`
-if [ $resp == 401 ];then
+
+i=0
+while [[ $i -lt 5 ]]
+do
+  if [ $resp == 401 ];then
     echo "Success"
-else
+    break
+  else
     echo "Not success - $resp"
-    exit 1
+    ((i++))
+    sleep 15
+  fi
+done
+
+if [[ $i -eq 5 ]]; then
+  echo "Validate service failed after 5 attempts"
 fi
+
+
+
+
+
 
