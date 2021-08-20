fe9f6afdbc605e302cb23f4e32711ba72e21e143
Kyla Marczewski <marczk@uw.edu>
2021-08-20 10:44:28 -0700

    add more silk profiling

--- app/measurement/views.py
+++ app/measurement/views.py
@@ -116,6 +116,7 @@ class MetricViewSet(MeasurementBaseViewSet):
     def get_queryset(self):
         return Metric.objects.all()
 
+    @silk_profile(name='Dispatch metrics')
     @method_decorator(cache_page(60 * 10))
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
