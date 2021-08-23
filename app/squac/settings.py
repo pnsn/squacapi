544d515c933d6ffdc8af8a927b350b33c8d3c2c2
Kyla Marczewski <marczk@uw.edu>
2021-08-23 10:17:20 -0700

    fix settings.pyst

--- app/squac/settings.py
+++ app/squac/settings.py
@@ -7,6 +7,7 @@ env variables config
 import requests
 from requests.exceptions import RequestException, MissingSchema
 import os
+from squac.cronjobs import CRONJOBS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,8 +40,7 @@ except RequestException or MissingSchema:
 # For debug toolbar
 INTERNAL_IPS = [
     'localhost',
-    '10.0.2.2',
-    '128.95.16.34'
+    '10.0.2.2'
 ]
 
 # tricks to have debug toolbar when developing with docker
