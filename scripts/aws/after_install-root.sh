b0a8a8be9c40d65782fd768e91c1582f792ab498
Kyla Marczewski <marczk@uw.edu>
2021-09-01 11:38:53 -0700

    s3 call

--- scripts/aws/after_install-root.sh
+++ scripts/aws/after_install-root.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-set -ex
+set -e
 set -o pipefail
 
 # install nginx conf files
-aws s3 cp s3://squacapi-config/nginx/* /etc/nginx/conf.d/ --recursive
+aws s3 cp s3://squacapi-config/nginx/squacapi.conf  /etc/nginx/conf.d/
+aws s3 cp s3://squacapi-config/nginx/staging-squacapi.conf  /etc/nginx/conf.d/
+aws s3 cp s3://squacapi-config/nginx/squacapi-testing.conf  /etc/nginx/conf.d/
 
 # install gunicorn service files
-aws s3 cp s3://squacapi-config/gunicorn/* /etc/systemd/system/ --recursive
-
+aws s3 cp s3://squacapi-config/gunicorn/gunicorn-production.service /etc/systemd/system/
+aws s3 cp s3://squacapi-config/gunicorn/gunicorn-staging.service /etc/systemd/system/
+aws s3 cp s3://squacapi-config/gunicorn/gunicorn-testing.service /etc/systemd/system/
 chown -R ec2-user:ec2-user /var/www/releases
 chmod 775 /var/www/releases
 
