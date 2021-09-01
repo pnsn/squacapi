6b99050926060bf3fde53c5bb907f4efa2054354
Kyla Marczewski <marczk@uw.edu>
2021-09-01 11:29:26 -0700

    use testing

--- scripts/aws/after_install-ec2-user.sh
+++ scripts/aws/after_install-ec2-user.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -ex
+set -o pipefail
 
 dest=/var/www/releases/$DEPLOYMENT_GROUP_NAME/$DEPLOYMENT_ID
 mkdir -p $dest
--- scripts/aws/application_start.sh
+++ scripts/aws/application_start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-set -e
+set -ex
 set -o pipefail
-
 
 export CURRENT_RELEASE=/var/www/releases/$DEPLOYMENT_GROUP_NAME/$DEPLOYMENT_ID
 export SYMLINK=/var/www/$DEPLOYMENT_GROUP_NAME
