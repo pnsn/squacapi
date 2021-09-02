e3c93284de4fffd859a76a0163b83728447cfd17
Kyla Marczewski <marczk@uw.edu>
2021-09-01 17:44:39 -0700

    app start change

--- scripts/aws/application_start.sh
+++ scripts/aws/application_start.sh
@@ -5,8 +5,6 @@ set -o pipefail
 
 export CURRENT_RELEASE=/var/www/releases/$DEPLOYMENT_GROUP_NAME/$DEPLOYMENT_ID
 export SYMLINK=/var/www/$DEPLOYMENT_GROUP_NAME
-
-cd $CURRENT_RELEASE
 # save previous pointer then repoint symlink
 
 # track the previous link
@@ -22,8 +20,7 @@ if [ $DEPLOYMENT_GROUP_NAME == 'squacapi' ]; then
 fi
 
 systemctl daemon-reload
-service $GUNICORN_SERVICE restart
-# PYTHONPATH=$SYMLINK service $GUNICORN_SERVICE restart || exit 1
+PYTHONPATH=$CURRENT_RELEASE service $GUNICORN_SERVICE restart || exit 1
 # restart nginx
 #only keep 5 versions
 cd  /var/www/releases/$DEPLOYMENT_GROUP_NAME && ls -A1t | tail -n +5 | xargs rm -rf
