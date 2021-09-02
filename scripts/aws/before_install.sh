d0a45d90019bc179683f9823fa141373ffb5174c
Kyla Marczewski <marczk@uw.edu>
2021-09-02 15:08:40 -0700

    make sure releases exists

--- scripts/aws/before_install.sh
+++ scripts/aws/before_install.sh
@@ -8,5 +8,7 @@ export APP_ROOT=/var/www/squacapi
 
 # Make tmp folder for codedeploy to place app
 mkdir -p $APP_ROOT/tmp
+mkdir -p $APP_ROOT/releases
+
 # Ensure there isn't an existing file here
 rm -rf $APP_ROOT/tmp/*
