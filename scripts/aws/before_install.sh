8cf59a4e786d9b022ecfed4dfd7f0215590a4b86
Kyla Marczewski <marczk@uw.edu>
2021-09-02 15:01:35 -0700

    remove spaces

--- scripts/aws/before_install.sh
+++ scripts/aws/before_install.sh
@@ -4,7 +4,7 @@ set -e
 set -o pipefail
 
 # Home base for the app
-export APP_ROOT = /var/www/squacapi
+export APP_ROOT=/var/www/squacapi
 
 # Make tmp folder for codedeploy to place app
 mkdir -p $APP_ROOT/tmp
