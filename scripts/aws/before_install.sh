9fb59441c760f51aeaad23335bbba43cc912c11e
Kyla Marczewski <marczk@uw.edu>
2021-09-03 13:40:03 -0700

    chance to tmp in releases

--- scripts/aws/before_install.sh
+++ scripts/aws/before_install.sh
@@ -9,4 +9,4 @@ APP_ROOT=/var/www/squacapi
 # Make tmp folder for codedeploy to place app
 mkdir -p $APP_ROOT/releases/$DEPLOYMENT_GROUP_NAME/tmp
 # Ensure there isn't an existing file here
-rm -rf $APP_ROOT/releases/$DEPLOYMENT_GROUP_NAME/tmp
+rm -rf $APP_ROOT/releases/tmp
