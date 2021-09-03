622f95d4586dc084d4935d9f710956f135088839
Kyla Marczewski <marczk@uw.edu>
2021-09-02 18:54:12 -0700

    move files to prevent clobbering

--- scripts/aws/before_install.sh
+++ scripts/aws/before_install.sh
@@ -7,7 +7,6 @@ set -o pipefail
 APP_ROOT=/var/www/squacapi
 
 # Make tmp folder for codedeploy to place app
-mkdir -p $APP_ROOT/releases/tmp
-
+mkdir -p $APP_ROOT/releases/$DEPLOYMENT_GROUP_NAME/tmp
 # Ensure there isn't an existing file here
-rm -rf $APP_ROOT/releases/tmp/
+rm -rf $APP_ROOT/releases/$DEPLOYMENT_GROUP_NAME/tmp
