7bb543535797c44adc0c3ae988a4f1700361d8d3
Kyla Marczewski <marczk@uw.edu>
2021-09-08 17:38:37 -0700

    fix crontab remove

--- scripts/aws/after_install-ec2-user.sh
+++ scripts/aws/after_install-ec2-user.sh
@@ -38,7 +38,7 @@ python $dest/app/manage.py migrate
 
 # if production, update cronjobs
 if [ $DEPLOYMENT_GROUP_NAME == 'production' ]; then
-    python $dest/app/manage.py crontab -r
+    python $dest/app/manage.py crontab remove
     python $dest/app/manage.py crontab add
 fi
 
