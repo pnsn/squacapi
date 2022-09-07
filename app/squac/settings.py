ab74547f7a159fa7b6806647daa99c00d73b790f
Kyla Marczewski <marczk@uw.edu>
2022-09-07 11:17:45 -0700

    allow pagination

--- app/squac/settings.py
+++ app/squac/settings.py
@@ -115,7 +115,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication'
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 SWAGGER_SETTINGS = {
