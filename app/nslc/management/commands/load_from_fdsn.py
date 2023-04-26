14d91c42b76e2bdb275f2ccda2af7d2a2de9219b
alexhutko <ahutko@gmail.com>
2023-04-26 16:28:17 -0700

    added networks, added availability check for waveforms in last year

--- app/nslc/management/commands/load_from_fdsn.py
+++ app/nslc/management/commands/load_from_fdsn.py
@@ -6,12 +6,12 @@ Command run without any options will give default URL
         &level=channel
         &net=AE,AG,AK,AV,AZ,BC,BK,CC,CE,CI,CN,CO,CU,ET,HV,IU,IW,LD,MB,N4,NC,NN,NP,NV,O2,OK,OO,PB,PR,SN,TX,UM,UO,US,UU,UW,WR,WY
         &sta=*
-        &cha=EN?,HN?,?H?
+        &cha=EN?,HN?,BH?,EH?,HH?
         &loc=*
         &minlat=12.
-        &maxlat=72.
+        &maxlat=85.
         &minlon=-183.
-        &maxlon=-62.
+        &maxlon=-52.
         &starttime=2019-01-01
         &format=text
 
@@ -67,9 +67,9 @@ class Command(BaseCommand):
         )
         parser.add_argument(
             '--cha',
-            default="EN?,HN?,?H?",
+            default="EN?,HN?,BH?,EH?,HH?",
             help="Comma separated regex for channel code, ? wildcard, \
-                EN?,HN?,?H? default"
+                EN?,HN?,BH?,EH?,HH? default"
         )
         parser.add_argument(
             '--loc',
@@ -83,8 +83,8 @@ class Command(BaseCommand):
         )
         parser.add_argument(
             '--maxlat',
-            default=72,
-            help="Upper latitude for search box, 72 default"
+            default=85,
+            help="Upper latitude for search box, 85 default"
         )
         parser.add_argument(
             '--minlon',
@@ -93,8 +93,8 @@ class Command(BaseCommand):
         )
         parser.add_argument(
             '--maxlon',
-            default=-62,
-            help="Right latitude for search box, -62 default"
+            default=-52,
+            help="Right latitude for search box, -52 default"
         )
         parser.add_argument(
             '--starttime',
@@ -128,13 +128,53 @@ class Command(BaseCommand):
             )
         return url
 
+    def check_availability(self, params):
+        ''' check FDSNWS availabilty service and return list of NSLC
+            for channels whose latest upload of waveform data is 
+            more than one year ago, i.e. list of NSLC that should not get 
+            added. Only applies to NLSCs with data in the past, i.e. 
+            ignores newly installed channels.
+            documentation at https://service.iris.edu/fdsnws/availability/1
+        '''
+        if ( params['net'] is not None ):
+            availability_url = (
+                f"https://service.iris.edu/fdsnws/availability/1/extent?"
+                f"&net={params['net']}"
+                "&format=text&nodata=404"
+
+            NSLC_skip = []
+            with requests.Session() as s:
+                download = s.get(availabililty_url)
+                decoded_content = download.content.decode('utf-8')
+                content = csv.reader(
+                    decoded_content.splitlines(), delimiter=' ')
+                row_list = list(content)
+                if ( row_list[0][0:8] == "#Network" ):
+                    # skip header rows in metadata
+                    for row in row_list[1:]:
+                        # extract data from row
+                    if len(row) == 11:
+                        net, sta, loc, cha, qua, samp, earlieststr, *rem = row
+                        lateststr, updated, timespans, restr = rem
+                        earliest = datetime.strptime(earlieststr.split("T")[0],
+                                   "%Y-%m-%d")
+                        latest = datetime.strptime(lateststr.split("T")[0],
+                                 "%Y-%m-%d")
+                        today = datetime.today()
+                        if ( (today-latest).days > 366 and (earliest < today) ):
+                            NSLC_skip.append(f'{net}.{sta}.{loc}.{cha}'.lower())
+            )
+        return NSLC_skip
+
     '''Django command to check network and channel tables with FDSN service'''
     def handle(self, *args, **options):
         ALLOWED_NETWORKS = [
-            "AE", "AK", "AV", "AZ", "BC", "BK", "CC", "CE", "CI", "CN", "CO",
-            "CU", "ET", "HV", "IU", "IW", "LD", "MB", "N4", "NC", "NN", "NP",
-            "NV", "O2", "OK", "OO", "PB", "PR", "SN", "TX", "UM", "UO", "US",
-            "UU", "UW", "WR", "WY"
+            "AE", "AK", "AV", "AZ", "BC", "BK", "C0", "CC", "CE", "CI", "CN", "CO",
+            "CU", "ET", "GM", "GM", "HV", "IE", "IU", "IW", "KY", "LB", "LD", "LI",
+            "LM", "MB", "MU", "MX", "N4", "NC", "NE", "NN", "NP", "NV", "NW", "NX",
+            "NY", "O2", "OH", "OK", "OO", "PB", "PE", "PO", "PR", "PT", "PY", "RB",
+            "RC", "RE", "SB", "SC", "SN", "TR", "TX", "UM", "UO", "US", "UU", "UW",
+            "WR", "WU", "WW", "WY"
         ]
         options["net"] = ','.join(ALLOWED_NETWORKS)
         LOADER_EMAIL = os.environ.get('LOADER_EMAIL')
@@ -197,6 +237,7 @@ class Command(BaseCommand):
         channel_url = self.build_url(options, 'channel')
         channels = {}
         with requests.Session() as s:
+            NSLC_skip = self.check_availability(options)
             download = s.get(channel_url)
             decoded_content = download.content.decode('utf-8')
             content = csv.reader(
@@ -231,30 +272,32 @@ class Command(BaseCommand):
                         channels[nslc_code] = {'start': start_datetime,
                                                'end': end_datetime}
                     try:
-                        # Update or create the channel using data
-                        Channel.objects.update_or_create(
-                            network=Network.objects.get(code=net.lower()),
-                            station_code=sta.lower(),
-                            loc='--' if not loc else loc.lower(),
-                            code=cha.lower(),
-                            defaults={
-                                'lat': float(lat),
-                                'lon': float(lon),
-                                'elev': float(elev),
-                                'depth': float(depth),
-                                'name': stations[sta.lower()],
-                                'azimuth': 0.0 if not azimuth else float(
-                                    azimuth),
-                                'dip': 0.0 if not dip else float(dip),
-                                'sensor_description': sensor_descr,
-                                'scale': 0.0 if not scale else float(scale),
-                                'scale_freq': 0.0 if not freq else float(freq),
-                                'scale_units': units,
-                                'sample_rate': 0.0 if not rate else float(
-                                    rate),
-                                'starttime': channels[nslc_code]['start'],
-                                'endtime': channels[nslc_code]['end'],
-                                'user': user,
+                        # Update or create the channel using data.
+                        # Skip if no waveform data for > 1 yr
+                        if ( nslc_code not in NSLC_skip ):
+                            Channel.objects.update_or_create(
+                                network=Network.objects.get(code=net.lower()),
+                                station_code=sta.lower(),
+                                loc='--' if not loc else loc.lower(),
+                                code=cha.lower(),
+                                defaults={
+                                    'lat': float(lat),
+                                    'lon': float(lon),
+                                    'elev': float(elev),
+                                    'depth': float(depth),
+                                    'name': stations[sta.lower()],
+                                    'azimuth': 0.0 if not azimuth else float(
+                                        azimuth),
+                                    'dip': 0.0 if not dip else float(dip),
+                                    'sensor_description': sensor_descr,
+                                    'scale': 0.0 if not scale else float(scale),
+                                    'scale_freq': 0.0 if not freq else float(freq),
+                                    'scale_units': units,
+                                    'sample_rate': 0.0 if not rate else float(
+                                        rate),
+                                    'starttime': channels[nslc_code]['start'],
+                                    'endtime': channels[nslc_code]['end'],
+                                    'user': user,
                             }
                         )
                     except KeyError as e:
