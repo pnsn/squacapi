53acb463ad4ca7234e0c8df53853cbe11d223ec3
Kyla Marczewski <marczk@uw.edu>
2023-12-05 12:15:58 -0800

    add health check middleware

--- app/squac/settings.py
+++ app/squac/settings.py
@@ -99,6 +99,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'squac.middleware.HealthCheckMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
